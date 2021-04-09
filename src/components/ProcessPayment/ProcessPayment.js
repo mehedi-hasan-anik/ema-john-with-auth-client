ca14b6bcd474f5b3efe404032d4720eb7b506173
mehedi-hasan-anik <anikm2283@gmail.com>
2021-04-10 01:00:03 +0600

    add

---
+++ src/components/ProcessPayment/ProcessPayment.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import SimpleCardForm from './SimpleCardForm';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_51Ie122BgoNPnJ9PsOBsvki1BkTcQVp6b39eTR7b3rvDMx3iYWAVreWIxJiXADDnILvmJPQ4Mn8YsN7HLm6H1aTcP00gLGZhD34');
+
+const ProcessPayment = () => {
+    return (
+        <Elements stripe={stripePromise}>
+           <SimpleCardForm></SimpleCardForm>
+      </Elements>
+    );
+};
+
+export default ProcessPayment;
